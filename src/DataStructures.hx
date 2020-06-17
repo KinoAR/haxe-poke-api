7fabfaceb12ac32eaf7401e6544e904c7915678f
Kino  Rose <kinoarose@gmail.com>
2020-06-16 21:26:56 -0400

    Add additional API calls into the poke API

--- src/DataStructures.hx
+++ src/DataStructures.hx
@@ -1,12 +1,42 @@
 
+/**
+ * Named API Resource
+ * name is the name
+ * url refers to a link to another resource
+ */
+
+typedef Url = {
+  url:String
+}
+
 typedef NameUrl = {
   name:String, 
   url:String
 }
 
+typedef Description = {
+  description:String,
+  language:NameUrl
+}
+
 typedef LangNames = {
    name:String,
    language:NameUrl
+}
+
+typedef PokemonEntry = {
+  entry_number:Int,
+  pokemon_species:NameUrl
+}
+
+typedef Effect = {
+  effect:String,
+  language:LangNames
+}
+
+typedef FlavorText = {
+  flavor_text:String,
+  language:LangNames
 }
 
 typedef Berry = {
@@ -49,5 +79,210 @@ typedef Contest = {
   id:Int,
   name:String,
   berry_flavor:NameUrl,
+  names:Array<{
+    name:String,
+    color:String,
+    language:NameUrl
+  }>
+}
+
+typedef ContestEffects = {
+  id:Int,
+  appeal:Int,
+  jam:Int,
+  effect_entries:Array<Effect>,
+  flavor_text_entries:Array<FlavorText>
+}
+
+typedef SuperContestEffects = {
+  id:Int,
+  appeal:Int,
+  flavor_text_entries:Array<FlavorText>,
+  moves:Array<NameUrl>
+}
+
+typedef Encounter = {
+  id:Int,
+  name:String,
+  order:Int,
   names:Array<LangNames>
+}
+
+typedef EncounterConditions = {
+  id:Int,
+  name:String,
+  values:Array<NameUrl>,
+  names:Array<LangNames>
+}
+
+typedef EncounterConditionValues = {
+  id:Int,
+  name:String,
+  condition:NameUrl,
+  names:Array<LangNames>
+}
+
+
+typedef ChainLink = {
+  is_baby:Bool,
+  species:NameUrl,
+  evolution_details:Array<EvolutionDetails>,
+  evolves_to:Array<ChainLink>
+}
+
+typedef EvolutionDetails = {
+  item:NameUrl,
+  trigger:NameUrl,
+  gender:Int,
+  held_item:NameUrl,
+  known_move:NameUrl,
+  min_level:Int,
+  min_happiness:Int,
+  min_beauty:Int,
+  min_affection:Int,
+  needs_overworld_rain:Bool,
+  time_of_day:String,
+  /**
+   * 1  = Attack > Defense
+   * 0 = Attack == Defense
+   * -1 = Attack < Defense
+   */
+  relative_physical_stats:Int, 
+  turn_upside_down:Bool
+}
+
+typedef EvolutionChains = {
+  id:Int,
+  baby_trigger_item:String,
+  chain:ChainLink 
+}
+
+typedef EvolutionTrigger = {
+  id:Int,
+  name:String,
+  names:Array<LangNames>,
+  pokemon_species:Array<NameUrl>
+}
+
+typedef Generation = {
+  id:Int,
+  name:String,
+  abilities:Array<NameUrl>,
+  names:Array<LangNames>,
+  main_region:NameUrl,
+  moves:Array<NameUrl>,
+  pokemon_species:Array<NameUrl>,
+  types:Array<NameUrl>,
+  version_groups:Array<NameUrl>
+}
+
+typedef PokedexT = {
+  id:Int,
+  name:String,
+  is_main_series:Bool,
+  description: Description,
+  names:Array<LangNames>,
+  pokemon_entries:Array<PokemonEntry>,
+  region:NameUrl,
+  version_groups:Array<NameUrl>
+}
+
+typedef Version = {
+  id:Int,
+  name:String,
+  names:Array<LangNames>,
+  version_group:Array<NameUrl>
+}
+
+typedef VersionGroup = {
+  id:Int,
+  name:String,
+  order:Int,
+  generation:NameUrl,
+  move_learn_methods:Array<NameUrl>,
+  pokedexes:Array<NameUrl>,
+  regions:Array<NameUrl>,
+  versions:Array<NameUrl>
+}
+
+typedef VersionGroupFlavorText = {
+  text:String,
+  version_group:NameUrl,
+  language:NameUrl,
+  
+}
+
+typedef MachineVersionDetail = {
+  machine:Url,
+  version_group:NameUrl
+}
+
+typedef Item = {
+  id:Int,
+  name:String,
+  cost:Int,
+  fling_power:Int,
+  fling_effect:NameUrl,
+  attributes:Array<NameUrl>,
+  category:NameUrl,
+  effect_entries:Array<{
+    effect:String,
+    short_effect:String,
+    language:NameUrl
+  }>,
+  flavor_text_entries:Array<VersionGroupFlavorText>,
+  game_indices:Array<{
+    game_index:Int,
+    generation:NameUrl
+  }>,
+  names:Array<LangNames>,
+  sprites: {
+    default_:String
+  },
+  held_by_pokemon:Array<{
+    pokemon:NameUrl,
+    version_details:Array<{
+      rarity:Int,
+      version:NameUrl
+    }>
+  }>,
+  baby_trigger_for: Url,
+  machines:Array<MachineVersionDetail>
+}
+
+typedef ItemAttributes = {
+  id:Int,
+  name:String,
+  description:Array<Description>,
+  items:Array<NameUrl>,
+  names:Array<LangNames>
+}
+
+typedef ItemCategory = {
+  id:Int,
+  name:String,
+  items:Array<NameUrl>,
+  names:Array<LangNames>,
+  pocket:NameUrl
+}
+
+typedef ItemFlingEffect = {
+  id:Int,
+  name:String,
+  effect_entries:Array<Effect>,
+  items:Array<NameUrl>
+}
+
+typedef ItemPocket = {
+  id:Int,
+  name:String,
+  categories:Array<NameUrl>,
+  names:Array<LangNames>
+}
+
+typedef Machine = {
+  id:Int,
+  item:NameUrl,
+  move:NameUrl,
+  version_group:NameUrl
 }
--- src/Pokedex.hx
+++ src/Pokedex.hx
@@ -1,5 +1,11 @@
+import haxe.ds.Either;
 import js.lib.Promise;
 import DataStructures;
+import haxe.extern.Rest;
+import haxe.extern.EitherType;
+
+typedef EitherT<T1, T2> = EitherType<T1, T2>;
+//EitherT<String, Array<Dynamic>>;
 
 typedef APIOptions = {
   protocol:String,
@@ -12,8 +18,24 @@ typedef APIOptions = {
 @:jsRequire("pokedex-promise-v2") 
 extern class Pokedex {
   public function new(?options:APIOptions):Void;
-  public function getBerryByName(berryName:String) : Promise<Berry>;
-  public function getBerryFirmnessByName(firmness:String) : Promise<BerryFirmness>;
-  public function getBerryFlavorByName(flavor:String) : Promise<BerryFlavor>;
-  public function getContestTypeByName(contestName:String) : Promise<Contest>;
+  public function getBerryByName(val: EitherT<String, Array<Dynamic>>) : Promise<EitherT<Berry, Array<Berry>>>;
+  public function getBerryFirmnessByName(firmness:EitherT<String, Array<Dynamic>>) : Promise<EitherT<BerryFirmness, Array<BerryFirmness>>>;
+  public function getBerryFlavorByName(flavor:EitherT<String, Array<Dynamic>>) : Promise<EitherT<BerryFlavor, Array<BerryFlavor>>>;
+  public function getContestTypeByName(contestName:EitherT<String, Array<Dynamic>>) : Promise<EitherT<Contest, Array<Contest>>>;
+  public function getSuperContestEffectById(id:Int) : Promise<EitherT<SuperContestEffects,Array<SuperContestEffects>>>;
+  public function getEncounterMethodByName(encounterMethod:EitherT<String, Array<Dynamic>>) : Promise<EitherT<Encounter, Array<Encounter>>>;
+  public function getEncounterConditionByName(encounterCondName:EitherT<String, Array<Dynamic>>) : Promise<EitherT<EncounterConditions, Array<EncounterConditions>>>;
+  public function getEncounterConditionValueByName(encounterCondValue:EitherT<String, Array<Dynamic>>) : Promise<EitherT<EncounterConditionValues, Array<EncounterConditionValues>>>;
+  public function getEvolutionChainById(id:Int) : Promise<EvolutionChains>;
+  public function getEvolutionTriggerByName(name:EitherT<String, Array<Dynamic>>) : Promise<EitherT<EvolutionTrigger, Array<EvolutionTrigger>>>;
+  public function getGenerationByName(name:EitherT<String, Array<Dynamic>>) : Promise<EitherT<Generation, Array<Generation>>>;
+  public function getPokedexByName(name:EitherT<String, Array<Dynamic>>) : Promise<EitherT<PokedexT, Array<PokedexT>>>;
+  public function getVersionByName(name:EitherT<String, Array<Dynamic>>) : Promise<EitherT<Version, Array<Version>>>;
+  public function getVersionGroupyByName(name:EitherT<String, Array<Dynamic>>) : Promise<EitherT<VersionGroup, Array<VersionGroup>>>;
+  public function getItemByName(name:EitherT<String, Array<Dynamic>>) : Promise<EitherT<Item, Array<Item>>>;
+  public function getItemAttributesByName(name: EitherT<String, Array<Dynamic>>) : Promise<EitherT<ItemAttributes, Array<ItemAttributes>>>;
+  public function getItemCategoryByName(name:EitherT<String, Array<Dynamic>>) : Promise<EitherT<ItemCategory, Array<ItemCategory>>>;
+  public function getItemFlingEffectByName(name:EitherT<String, Array<Dynamic>>) : Promise<EitherT<ItemFlingEffect, Array<ItemFlingEffect>>>;
+  public function getItemPocketByName(name:EitherT<String, Array<Dynamic>>) : Promise<EitherT<ItemPocket, Array<ItemPocket>>>;
+  public function getMachineById(id:Int) : Promise<EitherT<Machine, Array<Machine>>>;
 }
